22213a67ef924725a6724ad097252f0d09a26f51
ValentinJDT <vjeandot@yahoo.com>
2021-09-19 21:08:04 +0200

    Change the title of the private chest and more

--- src/main/java/antikbase/events/PlaceChestEvent.java
+++ src/main/java/antikbase/events/PlaceChestEvent.java
@@ -1,6 +1,8 @@
 package antikbase.events;
 
+import antikbase.recipes.Recipes;
 import org.bukkit.block.Chest;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -15,13 +17,17 @@ public class PlaceChestEvent implements Listener {
         if(!(e.getBlock().getState() instanceof Chest)) return;
         if(!e.getItemInHand().hasItemMeta()) return;
 
+        Player player = e.getPlayer();
+
         ItemStack itemStack = e.getItemInHand();
         ItemMeta itemMeta = itemStack.getItemMeta();
 
+        ItemStack privateChest = Recipes.getPrivateChest();
 
+        if(!itemMeta.equals(privateChest.getItemMeta())) return;
 
         Chest chest = (Chest) e.getBlock().getState();
-        chest.setCustomName("[Privé]");
+        chest.setCustomName("§7[§cPrivé§7] §7" + player.getName());
         chest.update();
     }
 }
